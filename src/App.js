983051d621975e0ce904896a6631403dff7915f2
Fernando <fernandolopezchs11@gmail.com>
2024-09-15 14:04:18 -0700

    using hash router.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles/utilities.css';
 import Header from './components/Header/Header';
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
-import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import {Route, HashRouter as Router, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Map from "./pages/Map/Map";
 import Footer from "./components/Footer/Footer";
@@ -17,7 +17,6 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/contact" element={<Contact/>}/>
-                <Route path="/map" element={<Map/>}/>
             </Routes>
         </Router>
     );
